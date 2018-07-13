6e075669405fce24a943cf5dc882302bbbdc90c5
Ardhi Lukianto <ardhi@lukianto.com>
2018-07-13 15:43:02 +0700

    reg update

--- boot.js
+++ boot.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const Router = require('koa-router')
-const session = require('koa-session')
 
 module.exports = function(cuk){
   let pkgId = 'route',
@@ -14,26 +13,29 @@ module.exports = function(cuk){
   pkg.lib.Router = Router
 
   return new Promise((resolve, reject) => {
-    app.use(session(pkg.cfg.common.session, app))
-//    app.use(makeDefHandler())
-    app.use(helper('http:composeMiddleware')('http:responseTime', `${pkgId}:*`))
+    require('./lib/session')(cuk)
+    .then(() => {
+  //    app.use(makeDefHandler())
+      app.use(helper('http:composeMiddleware')('http:responseTime', `${pkgId}:*`))
 
-    helper('core:bootTrace')('%A Loading routes...', null)
-    helper('core:bootDeep')({
-      pkgId: pkgId,
-      name: '',
-      parentAction: opts => {
-        let opt = opts.pkg.cfg.common.mount === '/' ? null : { prefix: opts.pkg.cfg.common.mount }
-        let router = new Router(opt)
-        app.use(helper('http:composeMiddleware')(_.get(pkg.cfg, 'cuks.http.middleware', []), `${pkgId}:${opts.pkg.id}`, true))
-        _.each(opts.files, f => {
-          makeRoute(f, opts.pkg, pkg, router, opts.dir)
-        })
-        app
+      helper('core:bootTrace')('%A Loading routes...', null)
+      helper('core:bootDeep')({
+        pkgId: pkgId,
+        name: '',
+        parentAction: opts => {
+          let opt = opts.pkg.cfg.common.mount === '/' ? null : { prefix: opts.pkg.cfg.common.mount }
+          let router = new Router(opt)
+          app.use(helper('http:composeMiddleware')(_.get(pkg.cfg, 'cuks.http.middleware', []), `${pkgId}:${opts.pkg.id}`, true))
+          _.each(opts.files, f => {
+            makeRoute(f, opts.pkg, pkg, router, opts.dir)
+          })
+          app
           .use(router.routes())
           .use(router.allowedMethods())
-      }
+        }
+      })
+      resolve(true)
     })
-    resolve(true)
+    .catch(reject)
   })
 }
